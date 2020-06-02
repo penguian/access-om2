0f3e5a263efcedad2caa5a9b2c6e71a035242c76
Paul Leopardi <paul.leopardi@anu.edu.au>
2020-06-02 14:33:33 +1000

    Add all CICE5.1 configurations used to test at 0.1 degrees

--- install.sh
+++ install.sh
@@ -14,7 +14,23 @@ if [[ -z "${ACCESS_OM_DIR}" ]]; then
 fi
 export LIBACCESSOM2_ROOT=$ACCESS_OM_DIR/src/libaccessom2
 
-declare -a exepaths=(${ACCESS_OM_DIR}/src/mom/exec/nci/ACCESS-OM/fms_ACCESS-OM.x ${LIBACCESSOM2_ROOT}/build/bin/yatm.exe ${ACCESS_OM_DIR}/src/cice5/build_auscom_360x300_24p/cice_auscom_360x300_24p.exe ${ACCESS_OM_DIR}/src/cice5/build_auscom_1440x1080_480p/cice_auscom_1440x1080_480p.exe ${ACCESS_OM_DIR}/src/cice5/build_auscom_3600x2700_722p/cice_auscom_3600x2700_722p.exe ${ACCESS_OM_DIR}/src/mom/bin/mppnccombine.nci)
+declare -a exepaths=(${ACCESS_OM_DIR}/src/mom/exec/nci/ACCESS-OM/fms_ACCESS-OM.x ${LIBACCESSOM2_ROOT}/build/bin/yatm.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_360x300_24p/cice_auscom_360x300_24p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_1440x1080_480p/cice_auscom_1440x1080_480p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_0361_045x045_3600x2700_361p/cice_auscom_0361_045x045_3600x2700_361p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_0361_060x060_3600x2700_361p/cice_auscom_0361_060x060_3600x2700_361p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_0361_090x090_3600x2700_361p/cice_auscom_0361_090x090_3600x2700_361p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_0361_120x135_3600x2700_361p/cice_auscom_0361_120x135_3600x2700_361p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_0722_060x060_3600x2700_722p/cice_auscom_0722_060x060_3600x2700_722p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_0722_090x090_3600x2700_722p/cice_auscom_0722_090x090_3600x2700_722p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_1392_090x090_3600x2700_1392p/cice_auscom_1392_090x090_3600x2700_1392p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_1444_060x060_3600x2700_1444p/cice_auscom_1444_060x060_3600x2700_1444p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_1444_090x090_3600x2700_1444p/cice_auscom_1444_090x090_3600x2700_1444p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_1444_120x135_3600x2700_1444p/cice_auscom_1444_120x135_3600x2700_1444p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_2888_090x090_3600x2700_2888p/cice_auscom_2888_090x090_3600x2700_2888p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_2888_120x135_3600x2700_2888p/cice_auscom_2888_120x135_3600x2700_2888p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_2888_180x180_3600x2700_2888p/cice_auscom_2888_180x180_3600x2700_2888p.exe \
+${ACCESS_OM_DIR}/src/mom/bin/mppnccombine.nci)
 # ${ACCESS_OM_DIR}/src/matm/build_nt62/matm_nt62.exe
 # ${ACCESS_OM_DIR}/src/matm/build_jra55/matm_jra55.exe
 
@@ -42,8 +58,42 @@ echo "Compiling CICE5.1 at 1 degree..."
 make # 1 degree
 echo "Compiling CICE5.1 at 1/4 degree..."
 make 025deg
-echo "Compiling CICE5.1 at 1/10 degree..."
-make 01deg
+# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 361 ..."
+# make 01deg_0361
+# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 722 ..."
+# make 01deg_0722
+# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 1392..."
+# make 01deg_1392
+# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 1444 ..."
+# make 01deg_1444
+# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 2888 ..."
+# make 01deg_2888
+echo "Compiling CICE5.1 at 1/10 degree using auscom_0361_045x045..."
+make 01deg_0361_045x045
+echo "Compiling CICE5.1 at 1/10 degree using auscom_0361_060x060..."
+make 01deg_0361_060x060
+echo "Compiling CICE5.1 at 1/10 degree using auscom_0361_090x090..."
+make 01deg_0361_090x090
+echo "Compiling CICE5.1 at 1/10 degree using auscom_0361_120x135..."
+make 01deg_0361_120x135
+echo "Compiling CICE5.1 at 1/10 degree using auscom_0722_060x060..."
+make 01deg_0722_060x060
+echo "Compiling CICE5.1 at 1/10 degree using auscom_0722_090x090..."
+make 01deg_0722_090x090
+echo "Compiling CICE5.1 at 1/10 degree using auscom_1392_090x090..."
+make 01deg_1392_090x090
+echo "Compiling CICE5.1 at 1/10 degree using auscom_1444_060x060..."
+make 01deg_1444_060x060
+echo "Compiling CICE5.1 at 1/10 degree using auscom_1444_090x090..."
+make 01deg_1444_090x090
+echo "Compiling CICE5.1 at 1/10 degree using auscom_1444_120x135..."
+make 01deg_1444_120x135
+echo "Compiling CICE5.1 at 1/10 degree using auscom_2888_090x090..."
+make 01deg_2888_090x090
+echo "Compiling CICE5.1 at 1/10 degree using auscom_2888_120x135..."
+make 01deg_2888_120x135
+echo "Compiling CICE5.1 at 1/10 degree using auscom_2888_180x180..."
+make 01deg_2888_180x180
 
 echo "Checking all executables have been built..."
 for p in "${exepaths[@]}"
