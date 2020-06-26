1668aedefa7b632df0aa3ff36a8e7ec48569e3ee
Paul Leopardi <paul.leopardi@anu.edu.au>
2020-06-26 11:43:07 +1000

    Install correct executables

--- install.sh
+++ install.sh
@@ -17,22 +17,31 @@ export LIBACCESSOM2_ROOT=$ACCESS_OM_DIR/src/libaccessom2
 declare -a exepaths=(${ACCESS_OM_DIR}/src/mom/exec/nci/ACCESS-OM/fms_ACCESS-OM.x ${LIBACCESSOM2_ROOT}/build/bin/yatm.exe \
 ${ACCESS_OM_DIR}/src/cice5/build_auscom_360x300_24p/cice_auscom_360x300_24p.exe \
 ${ACCESS_OM_DIR}/src/cice5/build_auscom_1440x1080_480p/cice_auscom_1440x1080_480p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_0361_045x045_3600x2700_361p/cice_auscom_0361_045x045_3600x2700_361p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_0361_060x060_3600x2700_361p/cice_auscom_0361_060x060_3600x2700_361p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_0361_090x090_3600x2700_361p/cice_auscom_0361_090x090_3600x2700_361p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_0361_120x135_3600x2700_361p/cice_auscom_0361_120x135_3600x2700_361p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_0722_060x060_3600x2700_722p/cice_auscom_0722_060x060_3600x2700_722p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_0722_090x090_3600x2700_722p/cice_auscom_0722_090x090_3600x2700_722p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_1392_090x090_3600x2700_1392p/cice_auscom_1392_090x090_3600x2700_1392p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_1444_060x060_3600x2700_1444p/cice_auscom_1444_060x060_3600x2700_1444p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_1444_090x090_3600x2700_1444p/cice_auscom_1444_090x090_3600x2700_1444p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_1444_120x135_3600x2700_1444p/cice_auscom_1444_120x135_3600x2700_1444p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_2888_090x090_3600x2700_2888p/cice_auscom_2888_090x090_3600x2700_2888p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_2888_120x135_3600x2700_2888p/cice_auscom_2888_120x135_3600x2700_2888p.exe \
-${ACCESS_OM_DIR}/src/cice5/build_auscom_2888_180x180_3600x2700_2888p/cice_auscom_2888_180x180_3600x2700_2888p.exe \
+${ACCESS_OM_DIR}/src/cice5/build_auscom_3600x2700_722p/cice_auscom_3600x2700_722p.exe \
 ${ACCESS_OM_DIR}/src/mom/bin/mppnccombine.nci)
 # ${ACCESS_OM_DIR}/src/matm/build_nt62/matm_nt62.exe
 # ${ACCESS_OM_DIR}/src/matm/build_jra55/matm_jra55.exe
+
+cice_01_sizes="\
+0361_045x045_3600x2700_361p \
+0361_060x060_3600x2700_361p \
+0361_090x090_3600x2700_361p \
+0361_120x135_3600x2700_361p \
+0722_060x060_3600x2700_722p \
+0722_090x090_3600x2700_722p \
+1392_090x090_3600x2700_1392p \
+1444_060x060_3600x2700_1444p \
+1444_090x090_3600x2700_1444p \
+1444_120x135_3600x2700_1444p \
+2888_090x090_3600x2700_2888p \
+2888_120x135_3600x2700_2888p \
+2888_180x180_3600x2700_2888p"
+
+cice_01_exepaths=""
+for size in $cice_01_sizes
+do
+  cice_01_exepaths="${cice_01_exepaths} ${ACCESS_OM_DIR}/src/cice5/build_auscom_${size}/cice_auscom_${size}.exe"
+done
 
 cd ${ACCESS_OM_DIR}
 
@@ -40,7 +49,7 @@ cd ${ACCESS_OM_DIR}
 # ${ACCESS_OM_DIR}/get_input_data.py
 
 echo "Removing previous executables (if any)..."
-for p in "${exepaths[@]}"
+for p in "${exepaths[@]}" $cice_01_exepaths
 do
     rm ${p} && echo "rm ${p}"
 done
@@ -58,16 +67,8 @@ echo "Compiling CICE5.1 at 1 degree..."
 make # 1 degree
 echo "Compiling CICE5.1 at 1/4 degree..."
 make 025deg
-# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 361 ..."
-# make 01deg_0361
-# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 722 ..."
-# make 01deg_0722
-# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 1392..."
-# make 01deg_1392
-# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 1444 ..."
-# make 01deg_1444
-# echo "Compiling CICE5.1 at 1/10 degree with NPROCS = 2888 ..."
-# make 01deg_2888
+echo "Compiling CICE5.1 at 1/10 degree..."
+make 01deg
 echo "Compiling CICE5.1 at 1/10 degree using auscom_0361_045x045..."
 make 01deg_0361_045x045
 echo "Compiling CICE5.1 at 1/10 degree using auscom_0361_060x060..."
@@ -96,12 +97,17 @@ echo "Compiling CICE5.1 at 1/10 degree using auscom_2888_180x180..."
 make 01deg_2888_180x180
 
 echo "Checking all executables have been built..."
-for p in "${exepaths[@]}"
+for p in "${exepaths[@]}" $cice_01_exepaths
 do
     ls ${p} || { echo "Build failed!"; exit 1; }
 done
 
 source ${ACCESS_OM_DIR}/hashexe.sh
+
+for p in $cice_01_exepaths
+do
+    cp -a ${p} ${ACCESS_OM_DIR}/bin && echo "cp -a ${p} ${ACCESS_OM_DIR}/bin"
+done
 
 cd ${ACCESS_OM_DIR}
 echo "Executables were built using these library versions:"
